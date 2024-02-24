bf0eb0af60697b77c577a1fe4b6839133a8e2f44
Atreids <56371059+atreids@users.noreply.github.com>
2024-02-24 12:57:03 +0000

    feat: basic start of vue project

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <TheHeader />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    flex-direction: column;
+    max-width: 30vw;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
